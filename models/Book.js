3f37a96761b86fbb2962074f03e674c30d8c2bd1
Achyut <actimsina@gmail.com>
2023-04-27 12:49:44 +0545

    embedded reviews

--- models/Book.js
+++ models/Book.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose')
 
+const reviewSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true,
+        minLength: 10
+    }
+})
+
 const bookSchema = new mongoose.Schema({
     title: {
         type: String,
@@ -8,7 +16,8 @@ const bookSchema = new mongoose.Schema({
     author: {
         type: String,
         default: 'Anonymous'
-    }
+    },
+    reviews: [reviewSchema]
 }, { timestamps: true })
 
 module.exports = mongoose.model('Book', bookSchema)
